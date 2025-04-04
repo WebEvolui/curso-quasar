0ea69e2ac628e00268c570a52a7405e98cb5b375
Ewerton Dutra <ewerton-speed@hotmail.com>
2025-04-04 16:08:41 -0300

    Aula 07 - Editamos as rotas

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
-import { defineRouter } from '#q-app/wrappers';
+import { defineRouter } from "#q-app/wrappers";
 import {
   createMemoryHistory,
   createRouter,
   createWebHashHistory,
   createWebHistory,
-} from 'vue-router';
-import routes from './routes';
+} from "vue-router";
+import routes from "./routes";
+import { Cookies } from "quasar";
+import { api } from "boot/axios";
 
 /*
  * If not building with SSR mode, you can
@@ -19,7 +21,9 @@ import routes from './routes';
 export default defineRouter(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
+    : process.env.VUE_ROUTER_MODE === "history"
+    ? createWebHistory
+    : createWebHashHistory;
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
@@ -29,6 +33,22 @@ export default defineRouter(function (/* { store, ssrContext } */) {
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.VUE_ROUTER_BASE),
+  });
+
+  Router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth) {
+      const token = Cookies.get("token_adm_minierp");
+
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+      if (token && token.trim().length > 0) {
+        next();
+      } else {
+        next({ name: "login" });
+      }
+    } else {
+      next();
+    }
   });
 
   return Router;
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,17 +1,37 @@
-import type { RouteRecordRaw } from 'vue-router';
+import type { RouteRecordRaw } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "",
+        name: "painel",
+        component: () => import("pages/IndexPage.vue"),
+      },
+    ],
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/login",
+    component: () => import("layouts/LoginLayout.vue"),
+    children: [
+      {
+        path: "",
+        name: "login",
+        component: () => import("pages/login/Login.vue"),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
   },
 ];
 
