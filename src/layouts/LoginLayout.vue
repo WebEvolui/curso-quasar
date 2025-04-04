11a3cc3dec4dae73e7c5f246e9164d02405aab28
Ewerton Dutra <ewerton-speed@hotmail.com>
2025-04-04 15:45:49 -0300

    Aula 05 - Definindo Layout login

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container class="container-fluid">
+      <router-view></router-view>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LoginLayout",
+});
+</script>
