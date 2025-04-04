fcd9adcde01de73970ea1280fb4ccf342de7395a
Ewerton Dutra <ewerton-speed@hotmail.com>
2025-04-04 15:54:50 -0300

    Aula 06 - Configurando o Axios

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,23 +1,42 @@
-import { defineBoot } from '#q-app/wrappers';
-import axios, { type AxiosInstance } from 'axios';
+import { defineBoot } from "#q-app/wrappers";
+import axios, { type AxiosInstance } from "axios";
+import { Cookies } from "quasar";
 
-declare module 'vue' {
+declare module "vue" {
   interface ComponentCustomProperties {
     $axios: AxiosInstance;
     $api: AxiosInstance;
   }
 }
 
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' });
+const token = Cookies.get("token_adm_minierp") || "";
 
-export default defineBoot(({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
+const api = axios.create({ baseURL: process.env.API_URL || "" });
+api.defaults.headers.common["Accept"] = "application/json";
+api.defaults.headers.common["Content-Type"] = "application/json";
+api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+export default defineBoot(({ router, app }) => {
+  api.interceptors.request.use(
+    function (config) {
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  api.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      if (error.response.status === 401) {
+        router.push({ name: "login" });
+      }
+      return Promise.reject(error);
+    }
+  );
 
   app.config.globalProperties.$axios = axios;
   // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
